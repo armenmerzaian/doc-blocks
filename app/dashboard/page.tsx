f24adb03498646c175b6fe242beefd6fc2d9b438
Armen Merzaian <armen.merzaian@gmail.com>
2024-10-17 16:28:44 -0400

    Reverted changes
    Mostly editor changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import AddItemForm from "./components/add-item-form/add-item-form";
 import { ListItems } from "./components/list-items/list-items";
 import { EditItemForm } from "./components/edit-item-form/edit-item-form";
 import { NAV_BAR_HEIGHT } from "../lib/constants/constants";
-import DashboardEditor from "./components/dashboard-editor/dashboard-editor";
 
 const DashboardPage = () => {
 
@@ -20,7 +19,6 @@ const DashboardPage = () => {
         <div className="space-y-4">
           <AddItemForm />
           <EditItemForm />
-          <DashboardEditor />
         </div>
       </div>
     </main>
