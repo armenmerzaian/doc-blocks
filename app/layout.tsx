c0c499851d8e462757a60c8f226d9f1e38493e3d
Armen Merzaian <armen.merzaian@gmail.com>
2024-10-18 02:45:38 -0400

    Fixed hydration error in browser console

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body>
           <ThemeProvider
             attribute="class"
@@ -24,7 +24,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-          <TopHeader />
+            <TopHeader />
             {children}
           </ThemeProvider>
         </body>
