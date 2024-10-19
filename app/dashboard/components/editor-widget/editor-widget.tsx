7603ddda95cb4c43b24039aee061bfd6a92f28c9
Armen M <amerzanian@myseneca.ca>
2024-10-19 12:42:40 -0400

    Added local storage support for Editor
    Installed dependencies for plate plugins

--- app/dashboard/components/editor-widget/editor-widget.tsx
+++ app/dashboard/components/editor-widget/editor-widget.tsx
@@ -4,26 +4,31 @@ import React, { ReactNode, HTMLAttributes } from "react";
 import {
   usePlateEditor,
   Plate,
+  PlateEditor,
 } from "@udecode/plate-common/react";
 import { Editor } from "@/components/plate-ui/editor";
 import { cn } from "@/lib/utils";
-
 interface EditorWidgetProps extends HTMLAttributes<HTMLElement> {
   className?: string;
   defaultValue?: any; //JSONContent
   children?: ReactNode;
 }
 
-const value = [
+const defVal = [
   {
-    type: "p",
     children: [
       {
         text: "This is editable plain text with react and history plugins, just like a <textarea>!",
       },
     ],
+    type: "p",
   },
 ];
+
+
+
+ const localValue =
+   typeof window !== "undefined" && localStorage.getItem("editorContent");
 
 const EditorWidget: React.FC<EditorWidgetProps> = ({
   className,
@@ -32,16 +37,21 @@ const EditorWidget: React.FC<EditorWidgetProps> = ({
   ...restProps
 }) => {
 
-  const editor = usePlateEditor({
-    value,
+  const editor: PlateEditor = usePlateEditor({
+    value: localValue ? JSON.parse(localValue) : defVal,
   });
 
   return (
-      <div className={cn(className,"mt-[72px] p-10")}>
-        <Plate editor={editor}>
-          <Editor placeholder="Type your message here." />
-        </Plate>
-      </div>
+    <div className={cn("mt-[72px] p-10", className)}>
+      <Plate
+        editor={editor}
+        onChange={({ value }) => {
+          localStorage.setItem("editorContent", JSON.stringify(value));
+        }}
+      >
+        <Editor placeholder="Type your message here." />
+      </Plate>
+    </div>
   );
 };
 
